59029a5d5f31f6088f41d2c3354f7a34942f47e9
namt00058-droid <namt00058@gmail.com>
2025-09-04 12:23:26 +0700

    Create tool.php

---
+++ tool.php
@@ -0,0 +1,8 @@
+<?php
+// tool.php - file chính để chạy module (không cần key)
+echo "==============================\n";
+echo " Tool PHP chạy thành công! \n";
+echo " Phiên bản: CROSS-V3 (No Key)\n";
+echo "==============================\n\n";
+
+include 'module.php'; // code chính đặt trong module.php
